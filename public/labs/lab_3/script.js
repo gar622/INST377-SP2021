2c676539462594dff454f62abca6d518ce6c6a41
gar622 <73448556+gar622@users.noreply.github.com>
2021-02-12 00:34:21 -0700

    First commit lab 3
    html good, js array not good, css progressing

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,39 @@
 /* Put your javascript in here */
+function arrayCarousel(){
+    const array1=[] 
+    const slide = document.querySelectorAll(".slider")
+    let currentSlideIndex = 0;
+
+    const init =(i) =>{
+        slide.forEach((slidee) => {
+            slide.style.display = "none"
+        })
+    }
+
+    slide[i].style.display="block"
+
+}
+
+document.addEventListener("DOMContentLoaded", init(currentSlide))
+   
+
+const forward = () =>{
+        currentSlideIndex >= slide.length -1? currentSlide =0: currentSlide++
+        init(currentSlide) 
+    }
+
+    const back = ()  => {
+        currentSlide <= 0? currentSlide = slides.length-1: currentSlide--
+        init(currentSlide)
+    }
+
+    document.querySelector(".forward").addEventListener("button",forward)
+    document.querySelector(".back").addEventListener("button", back)
+    
+    setInterval(() =>{
+        forward()
+    }, 5000);
+
+
+
+
