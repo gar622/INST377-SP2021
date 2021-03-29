6ed4bd07e983f99bda6601d7fe5928d1481fe621
gar622 <73448556+gar622@users.noreply.github.com>
2021-03-29 16:21:03 -0600

    commit 1 doody
    following tutorial -- localhost is serving wrong files? kinda annoying idk whats the deal

---
+++ public/doodle/script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50;
+    let doodlerBottomSpace = 150
+    let isGameOver = false
+
+    function createDoodler() {
+        grid.appendChild(doodller)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+
+    }
+    class Platform{
+        constructor(newPlatBottom){
+            this.bottom = newPlatBottom
+            this.left = math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual 
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+    function createPlatforms() {
+        for(let i = 0; i < platformCount; i++){
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform(newPlatBottom)
+        }
+    }
+
+    function start() {
+        if (!isGameOver){
+            createDoodler()
+            
+        }
+    }
+})
