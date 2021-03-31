b70d6291bd1c18b064c14b4d26eeba03f94fc016
gar622 <73448556+gar622@users.noreply.github.com>
2021-03-31 14:26:05 -0600

    Update script.js

--- public/doodle/script.js
+++ public/doodle/script.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
-        doodlerLeftSpace = platforms[0].visual
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
 
